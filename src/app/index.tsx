763c6cde0244063909cb9e6b82438c7a3a511cef
Hyeonsu Lee <incleaf@gmail.com>
2018-10-25 18:07:09 +0900

    Add Sentry for error tracking

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,22 @@
+import * as Sentry from '@sentry/browser';
 import * as React from 'react';
 import { render } from 'react-dom';
 
+
 import App from 'app/App';
 import { requestInitializeEnvironment } from 'app/services/environment';
-// import { initializeUser } from 'app/services/user';
 import { store } from 'app/store';
 import { setTabKeyFocus } from 'app/utils';
+import { env } from './config';
 
-// store.dispatch(initializeUser());
-store.dispatch(requestInitializeEnvironment());
+// if (env.isProduction) {
+  Sentry.init({
+    dsn: 'https://0bc859e1423a42dc8728690b03bcedf0@sentry.io/1307887',
+  });
+// }
+Sentry.captureMessage("TEST");
 
 setTabKeyFocus();
+store.dispatch(requestInitializeEnvironment());
+
 render(<App store={store} />, document.getElementById('app'));
